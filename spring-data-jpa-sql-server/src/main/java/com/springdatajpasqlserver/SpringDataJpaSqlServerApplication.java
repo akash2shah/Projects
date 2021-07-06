e4a9280aeefb41e2f1f77e01a3274809f327b84d
akash <akash2shah@gmail.com>
2021-07-06 18:38:00 +0530

    intial commit

---
+++ spring-data-jpa-sql-server/src/main/java/com/springdatajpasqlserver/SpringDataJpaSqlServerApplication.java
@@ -0,0 +1,13 @@
+package com.springdatajpasqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaSqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaSqlServerApplication.class, args);
+	}
+
+}
